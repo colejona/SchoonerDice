26f403dc321cbfe5ceb387f8ee09dae93d6632da
colejona <colejona@gmail.com>
2022-05-13 00:14:07 -0700

    scoreThreeOfAKind can score a scrambled three of a kind

--- src/__tests__/score-three-of-a-kind_test.js
+++ src/__tests__/score-three-of-a-kind_test.js
@@ -4,4 +4,8 @@ describe("scoreThreeOfAKind", () => {
     it("can score a good three of a kind", () => {
         expect(scoreThreeOfAKind([1, 1, 1, 7, 7])).toEqual(17);
     });
+
+    it("can score a scrambled three of a kind", () => {
+        expect(scoreThreeOfAKind([5, 3, 5, 4, 5])).toEqual(22);
+    });
 });
