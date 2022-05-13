3343613d7b513c92e0a1263768df4ee5dcf99325
colejona <colejona@gmail.com>
2022-05-13 00:28:14 -0700

    scoreSmallStraight can score a scrambled small straight

--- src/__tests__/score-small-straight_test.js
+++ src/__tests__/score-small-straight_test.js
@@ -4,4 +4,8 @@ describe("scoreSmallStraight", () => {
     it("can score a good small straight", () => {
         expect(scoreSmallStraight([2, 3, 4, 5, 7])).toEqual(30);
     });
+
+    it("can score a scrambled small straight", () => {
+        expect(scoreSmallStraight([3, 4, 5, 7, 2])).toEqual(30);
+    });
 });
