92fa2602cc81b19625b4d6c094661e9d493e2104
colejona <colejona@gmail.com>
2022-05-13 00:51:25 -0700

    scoreAllDifferent can score a scrambled all different

--- src/__tests__/score-all-different_test.js
+++ src/__tests__/score-all-different_test.js
@@ -4,4 +4,8 @@ describe("scoreAllDifferent", () => {
     it("can score a good all different", () => {
         expect(scoreAllDifferent([1, 2, 3, 4, 5])).toEqual(35);
     });
+
+    it("can score a scrambled all different", () => {
+        expect(scoreAllDifferent([2, 4, 1, 8, 7])).toEqual(35);
+    });
 });
