d1a07e90b3cd8e1ea31886135b818cff498d8f33
colejona <colejona@gmail.com>
2022-05-12 22:59:36 -0700

    Can score a good scrambled full house

--- src/__tests__/schooner-dice_test.js
+++ src/__tests__/schooner-dice_test.js
@@ -5,4 +5,8 @@ describe("SchoonerDice", () => {
     it("can score a good " + Category.FULL_HOUSE, () => {
         expect(SchoonerDice.score(Category.FULL_HOUSE, [1, 1, 1, 7, 7])).toEqual(25);
     });
+
+    it ("can score a good scrambled " + Category.FULL_HOUSE, () => {
+        expect(SchoonerDice.score(Category.FULL_HOUSE, [3, 5, 3, 5, 5])).toEqual(25);
+    });
 });
