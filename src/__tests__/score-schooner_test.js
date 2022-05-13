d81824fd6d2085bd2636ffd208ea2fe8e8302d4a
colejona <colejona@gmail.com>
2022-05-13 01:25:35 -0700

    scoreSchooner can score an unqualified Schooner

--- src/__tests__/score-schooner_test.js
+++ src/__tests__/score-schooner_test.js
@@ -8,4 +8,8 @@ describe("scoreSchooner", () => {
     it("can score another good Schooner", () => {
         expect(scoreSchooner([5, 5, 5, 5, 5])).toEqual(50);
     });
+
+    it("can score an unqualified Schooner", () => {
+        expect(scoreSchooner([5, 4, 5, 5, 5])).toEqual(0);
+    });
 });
--- src/score-schooner.js
+++ src/score-schooner.js
@@ -1,3 +1,33 @@
 exports.scoreSchooner = dice => {
-    return 50;
+    const {allNumbersTheSame} = dice.reduce(
+        (accumulator, nextNumber) => {
+            if (!accumulator.allNumbersTheSame) {
+                return accumulator;
+            }
+            if (accumulator.lastNumber === null) {
+                return {
+                    lastNumber: nextNumber,
+                    allNumbersTheSame: true
+                };
+            }
+            if (nextNumber === accumulator.lastNumber) {
+                return {
+                    lastNumber: nextNumber,
+                    allNumbersTheSame: true
+                };
+            }
+            return {
+                lastNumber: nextNumber,
+                allNumbersTheSame: false
+            };
+        }, {
+            lastNumber: null,
+            allNumbersTheSame: true
+        });
+
+    if (allNumbersTheSame) {
+        return 50;
+    } else {
+        return 0;
+    }
 };
