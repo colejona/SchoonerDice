4db1025ed20005dd95021b451310cd811ba12bc9
colejona <colejona@gmail.com>
2022-05-13 01:48:27 -0700

    Clean up

--- src/__tests__/score-n-of-a-kind_test.js
+++ src/__tests__/score-n-of-a-kind_test.js
@@ -1,27 +1,35 @@
 const {scoreNOfAKind} = require("../score-n-of-a-kind");
 
+function scoreThreeOfAKind(dice) {
+    return scoreNOfAKind(3, dice);
+}
+
+function scoreFourOfAKind(dice) {
+    return scoreNOfAKind(4, dice);
+}
+
 describe("scoreNOfAKind", () => {
     it("can score a good three of a kind", () => {
-        expect(scoreNOfAKind(3, [1, 1, 1, 7, 7])).toEqual(17);
+        expect(scoreThreeOfAKind([1, 1, 1, 7, 7])).toEqual(17);
     });
 
     it("can score a scrambled three of a kind", () => {
-        expect(scoreNOfAKind(3, [5, 3, 5, 4, 5])).toEqual(22);
+        expect(scoreThreeOfAKind([5, 3, 5, 4, 5])).toEqual(22);
     });
 
     it("can score an unqualified three of a kind", () => {
-        expect(scoreNOfAKind(3, [1, 3, 5, 4, 5])).toEqual(0);
+        expect(scoreThreeOfAKind([1, 3, 5, 4, 5])).toEqual(0);
     });
 
     it("can score a good four of a kind", () => {
-        expect(scoreNOfAKind(4, [1, 1, 1, 1, 7])).toEqual(11);
+        expect(scoreFourOfAKind([1, 1, 1, 1, 7])).toEqual(11);
     });
 
     it("can score a scrambled four of a kind", () => {
-        expect(scoreNOfAKind(4, [5, 5, 5, 4, 5])).toEqual(24);
+        expect(scoreFourOfAKind([5, 5, 5, 4, 5])).toEqual(24);
     });
 
     it("can score an unqualified four of a kind", () => {
-        expect(scoreNOfAKind(4, [5, 3, 5, 4, 5])).toEqual(0);
+        expect(scoreFourOfAKind([5, 3, 5, 4, 5])).toEqual(0);
     });
 });
