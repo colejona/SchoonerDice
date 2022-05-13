50dac70bd9c37a4db089f1aba708391cb93dec2e
colejona <colejona@gmail.com>
2022-05-13 01:04:15 -0700

    scoreLargeStraight can score a scrambled large straight

--- src/__tests__/score-large-straight_test.js
+++ src/__tests__/score-large-straight_test.js
@@ -4,4 +4,8 @@ describe("scoreLargeStraight", () => {
     it("can score a good large straight", () => {
         expect(scoreLargeStraight([2, 3, 4, 5, 6])).toEqual(40);
     });
+
+    it("can score a scrambled large straight", () => {
+        expect(scoreLargeStraight([3, 2, 6, 4, 5])).toEqual(40);
+    });
 });
