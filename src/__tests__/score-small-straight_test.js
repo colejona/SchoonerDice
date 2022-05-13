0407ae4673deefa5ba8d64e30a6a86648055fb9c
colejona <colejona@gmail.com>
2022-05-13 00:47:50 -0700

    scoreSmallStraight can score a small straight with a lower extra number

--- src/__tests__/score-small-straight_test.js
+++ src/__tests__/score-small-straight_test.js
@@ -5,6 +5,10 @@ describe("scoreSmallStraight", () => {
         expect(scoreSmallStraight([2, 3, 4, 5, 7])).toEqual(30);
     });
 
+    it("can score a small straight with a lower extra number", () => {
+        expect(scoreSmallStraight([2, 5, 6, 7, 8])).toEqual(30);
+    });
+
     it("can score a scrambled small straight", () => {
         expect(scoreSmallStraight([3, 4, 5, 7, 2])).toEqual(30);
     });
